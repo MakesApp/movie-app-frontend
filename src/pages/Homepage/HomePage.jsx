6ba1dec2debdb83ef44ceab44039025ea2392f8a
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-02-15 15:54:20 +0200

    nofar should take it from here

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -1,19 +1,11 @@
 import MovieList from "../../components/MovieList/MovieList";
+import { useGetLatestMoviesQuery } from "../../services/api/movieSlice";
 
 const HomePage=() =>{
-	const data=[{
-		poster:"https://images.pexels.com/photos/3526022/pexels-photo-3526022.jpeg?auto=compress&cs=tinysrgb&w=800",
-		title:"Movie 1",
-		year:"2020",
-		rating:"8.3"
-	},{		poster:"https://images.pexels.com/photos/3526022/pexels-photo-3526022.jpeg?auto=compress&cs=tinysrgb&w=800",
-	},{		poster:"https://images.pexels.com/photos/3526022/pexels-photo-3526022.jpeg?auto=compress&cs=tinysrgb&w=800",
-	},{		poster:"https://images.pexels.com/photos/3526022/pexels-photo-3526022.jpeg?auto=compress&cs=tinysrgb&w=800",
-	},{		poster:"https://images.pexels.com/photos/3526022/pexels-photo-3526022.jpeg?auto=compress&cs=tinysrgb&w=800",
-	},{		poster:"https://images.pexels.com/photos/3526022/pexels-photo-3526022.jpeg?auto=compress&cs=tinysrgb&w=800",
-	}];
+	const {data:latestMovies}=useGetLatestMoviesQuery();
+
 	return <>
-		<MovieList movies={data}/> 
+		{latestMovies&&<MovieList movies={latestMovies}/> }
 	</>;
 };
 
