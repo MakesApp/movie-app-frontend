f37e3aca51b03ef540f2e0a73722b14128fcbbd8
nethanel10 <nethanelbedok@gmail.com>
2023-02-19 13:03:06 +0200

    pagination completed

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { MOVIES_PER_PAGE } from "./constants";
-/* eslint-disable react/prop-types */
 const Pagination = ({ totalPage, pageNumber, path }) => {
 	return (
 		<>
@@ -8,14 +8,13 @@ const Pagination = ({ totalPage, pageNumber, path }) => {
 				<button disabled={parseInt(pageNumber) - 1 <= 0}>Previous</button>
 			</Link>
 			{[...Array(Math.ceil(totalPage / MOVIES_PER_PAGE))].map((x, i) => (
-				<Link
+				<PageLink
 					key={i}
-					style={{ color: parseInt(pageNumber) - 1 === i ? "red" : "blue" }}
-					className='button'
+					isSelected={parseInt(pageNumber) - 1 === i}
 					to={`${path}/${i + 1}`}
 				>
 					{i + 1}
-				</Link>
+				</PageLink>
 			))}
 			<Link to={`${path}/${parseInt(pageNumber) + 1}`}>
 				<button disabled={parseInt(pageNumber) + 1 > Math.ceil(totalPage / 10)}>
@@ -25,4 +24,8 @@ const Pagination = ({ totalPage, pageNumber, path }) => {
 		</>
 	);
 };
+
+const PageLink = styled(Link)`
+	color: ${(props) => (props.isSelected ? "red" : "blue")}
+`;
 export default Pagination;
