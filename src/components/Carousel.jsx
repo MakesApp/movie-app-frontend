7005c71fe1e92c1456aa5e55e71476d17d779b86
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-03-05 12:40:27 +0200

    change pakcage json

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,42 +4,34 @@ import './carousel.styles.css';
 import Slider from 'react-slick';
 import { CarouselStyling } from './Carousel.styles';
 const Carousel = ({
-  slidesToShow,
-  type,
-  slidesToScroll,
-  autoplay,
-  fade,
-  color,
-  activeDotColor,
-  dotColor,
-  children,
+	children,
 }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: slidesToShow,
-    slidesToScroll: slidesToScroll,
-    autoplay: autoplay,
-    autoplaySpeed: 3000,
-    fade: fade,
-    pauseOnHover: true,
-    lazyLoad: true,
-    adaptiveHeight: true,
-    afterChange: (current) => type === 'movieSlider' && setCurrentIndex(current),
-  };
+	const settings = {
+		dots: true,
+		infinite: true,
+		slidesToShow: slidesToShow,
+		slidesToScroll: slidesToScroll,
+		autoplay: autoplay,
+		autoplaySpeed: 3000,
+		fade: fade,
+		pauseOnHover: true,
+		lazyLoad: true,
+		adaptiveHeight: true,
+		afterChange: (current) => type === 'movieSlider' && setCurrentIndex(current),
+	};
 
-  return (
-    <>
-      <CarouselStyling color={color} activeDotColor={activeDotColor} dotColor={dotColor}>
-        <Slider {...settings}>
-          {children &&
+	return (
+		<>
+			<CarouselStyling color={color} activeDotColor={activeDotColor} dotColor={dotColor}>
+				<Slider {...settings}>
+					{children &&
             children.map((child) => React.cloneElement(child, { currentIndex }))}
-        </Slider>
-      </CarouselStyling>
-    </>
-  );
+				</Slider>
+			</CarouselStyling>
+		</>
+	);
 };
 
 export default Carousel;
